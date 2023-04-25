fcf6b270cbe80bce47e9d303f5734dc2f26a9b97
sushma4777 <sushmamarunilla@gmail.com>
2023-04-25 16:53:03 +0000

    tomcat first code added

---
+++ src/main/java/com/sample/webproject/App.java
@@ -0,0 +1,13 @@
+package com.sample.webproject;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
